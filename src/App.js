26b693cad8962dfd223f4831d8b36332e21b875d
Shivani6494 <bhattshivani64@gmail.com>
2025-11-07 16:00:38 -0500

    Initial commit add react router and navigation to pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import {Route, Link, Routes} from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav style={{padding : '10px' , background : "pink"}}>
+        <Link to="/" style={{marginRight : '10px'}}>Home</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
     </div>
   );
 }
